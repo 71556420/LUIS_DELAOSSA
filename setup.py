84779a92a846951fe681e17dd7479377b5c9bba0
71556420 <fernando.delaossa@est.iudigital.edu.co>
2025-02-24 23:46:03 +0000

    cargar instruccion_1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="pad",
     version="0.0.1",
     author="LUIS DE LA OSSA",
-    author_email="",
+    author_email="fernando.delaossa@est.iudigital.edu.co",
     description="",
     py_modules=["actividad_1"],
     install_requires=[
